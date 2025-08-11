219ee5444574cffff62f84a5bea0d12fa84a176b
mosf-bvg <2379689707@qq.com>
2025-08-11 11:36:48 +0800

    Update build-64.bat

--- deploy/win64/build-64.bat
+++ deploy/win64/build-64.bat
@@ -7,7 +7,7 @@ set STARTPWD=%CD%
 
 rem The first paths are for workstation builds, the last for CI
 if not exist "%QTDIR%" (
-    set QTDIR=%QT_ROOT_DIR%
+    set QTDIR=%Qt5_DIR%
 )
 if not exist "%QTDIR%" (
 @   echo Could not find Qt in %QTDIR%
@@ -71,4 +71,5 @@ meson test -C %BUILDDIR%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 set PATH=%ORIGINALPATH%
+
 
